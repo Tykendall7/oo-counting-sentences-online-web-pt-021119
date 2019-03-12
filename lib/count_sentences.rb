4ac1e1a2f09ba2e9f31afe7d04c0bb3f920f1071
Tymeckia Kendall <tykend@gmail.com>
2019-03-12 03:57:03 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,18 @@ require 'pry'
 class String
 
   def sentence?
-    
+    "Hi, my name is Sophie.".end_with?(".")
   end
 
   def question?
-
+      "What's your name?".end_with?("?")
   end
 
   def exclamation?
-
+      "Happy Halloween!".end_with?("!")
   end
 
   def count_sentences
-
+    "one. two. three?".count_sentences
   end
 end
